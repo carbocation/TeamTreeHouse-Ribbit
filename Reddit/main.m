3d999997dea2f00c4c403594659d27c2c19d035e
Christer Nordbø <cnordbo@gmail.com>
2013-08-10 11:23:42 +0200

    Initial Commit

---
+++ Reddit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Reddit
+//
+//  Created by Christer Nordbø on 10.08.13.
+//  Copyright (c) 2013 Christer Nordbø. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ RedditTests/RedditTests.m
@@ -0,0 +1,34 @@
+//
+//  RedditTests.m
+//  RedditTests
+//
+//  Created by Christer Nordbø on 10.08.13.
+//  Copyright (c) 2013 Christer Nordbø. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RedditTests : XCTestCase
+
+@end
+
+@implementation RedditTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
